575a9e92345fae75218d1ab4bf3f7e480e18a066
goldmaster401093221 <goldmaster401093221@gmail.com>
2025-07-16 06:44:31 +0300

    update with old one

--- components/chatwidget.js
+++ components/chatwidget.js
@@ -7,7 +7,7 @@ const ChatWidget = () => {
     if (window.chatWidgetScriptLoaded) return;
 
     window.ChatWidgetConfig = {
-      projectId: "686eead1bd32803190c8cfa8",
+      projectId: "686d8006614557af9759d21d",
     };
 
     const script = document.createElement('script');
@@ -29,30 +29,3 @@ const ChatWidget = () => {
 };
 
 export default ChatWidget;
-
-
-// const ChatWidget = () => {
-//   useEffect(() => {
-//     if (window.chatWidgetScriptLoaded) return;
-
-//     window.ChatWidgetConfig = {
-//       projectId: "686d8006614557af9759d21d",
-//     };
-
-//     const script = document.createElement('script');
-//     script.src = "https://storage.googleapis.com/cdwidget/dist/assets/js/main.js";
-//     script.async = true;
-//     document.body.appendChild(script);
-
-//     window.chatWidgetScriptLoaded = true;
-
-//     return () => {
-//       // Cleanup if needed
-//       document.body.removeChild(script);
-//       delete window.chatWidgetScriptLoaded;
-//       delete window.ChatWidgetConfig;
-//     };
-//   }, []);
-
-//   return <div id="cd-widget"></div>;
-// };
